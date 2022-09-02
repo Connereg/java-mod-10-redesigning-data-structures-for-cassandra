c23bcaebcee639abc81eb10d69f3faee07d1738b
Conner Gleason <connereg@optonline.net>
2022-09-02 17:03:52 -0400

    Still having issues using the DeckController with DeckNumber, Need to remove older non-cassandra oriented files

--- deck/src/main/java/com/example/deck/CardDeckRepository.java
+++ deck/src/main/java/com/example/deck/CardDeckRepository.java
@@ -2,5 +2,8 @@ package com.example.deck;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CardDeckRepository extends CrudRepository<CardDeck, Long> {
+    Optional<CardDeck> findFirstByOrderByPositionDesc();
 }
